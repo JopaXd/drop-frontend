54b5a8b79ca576ae23a4cdd9a921ebbc77023639
JopaXd <pavle_milanov@hotmail.com>
2021-05-29 17:21:57 +0200

    Forgot to stage these 2 :)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 
-import { HttpClientModule } from '@angular/common/http'
+import { HttpClientModule } from '@angular/common/http';
+import { DndDirective } from './dnd.directive'
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    DndDirective
   ],
   imports: [
     BrowserModule,
