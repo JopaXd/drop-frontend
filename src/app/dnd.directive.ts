65e5136983f96842219cb1e0ed1fa72257f36b5d
JopaXd <pavle_milanov@hotmail.com>
2021-05-29 15:51:28 +0200

    Added a lot of home page ui, added basic upload stuff.

---
+++ src/app/dnd.directive.ts
@@ -0,0 +1,25 @@
+import {
+  Directive,
+  Output,
+  Input,
+  EventEmitter,
+  HostBinding,
+  HostListener
+} from '@angular/core';
+
+@Directive({
+  selector: '[fileDnd]'
+})
+export class DndDirective {
+  @Output() fileDropped = new EventEmitter<any>();
+
+  // Drop listener
+  @HostListener('drop', ['$event']) public ondrop(evt) {
+    evt.preventDefault();
+    evt.stopPropagation();
+    let files = evt.dataTransfer.files;
+    if (files.length > 0) {
+      this.fileDropped.emit(files);
+    }
+  }
+}
