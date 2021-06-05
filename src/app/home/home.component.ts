67bfa346fc721ca488aaaa92bad4868e46bdc9c7
JopaXd <pavle_milanov@hotmail.com>
2021-06-05 19:48:33 +0200

    Added upload functionality, boostrap is now loaded from a local file instead of cdn.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import { HttpClient } from '@angular/common/http';
+import { Modal } from 'bootstrap';
 
 @Component({
   selector: 'app-home',
@@ -7,13 +10,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private toastr: ToastrService, private http: HttpClient) { }
 
   ngOnInit(): void {
   }
 
+  fileLink;
+
   onFileDropped($event){
-    console.log($event[0])
+    let file = $event[0]
+    //Converting the file size to megabytes.
+    let file_size_mb = file.size / 1000000
+    //Check if file is larger than 200MB.
+    if (file_size_mb > 200){
+      //Toast message.
+      this.toastr.error("File is too big! The file should be less than 200 MB!", "Error!", { positionClass: "toast-bottom-right" })
+    }
+    else{
+      let formData = new FormData();
+      formData.append("file", file)
+      let upload = this.http.post("http://localhost:8000/uploadfile/", formData)
+      upload.subscribe(data => {
+          if (data["success"] == true){
+            //Will figure out how to create a proper link in production..
+            this.fileLink = `http://localhost:8000/file/${data["file_id"]}`
+            let linkModal = new Modal(document.getElementById('linkModal'))
+            linkModal.show()
+          }
+          else{
+            console.log(data["error"])
+          }
+      })
+    }
   }
 
 }
